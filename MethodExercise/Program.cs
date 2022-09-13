5b012b8ccb74efa31d621d5b0bbefad0aa3be99b
ChrisJonesTC <cfjones91@gmail.com>
2022-09-13 12:03:28 -0400

    initial commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello - what is your first name?");
+var userName = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine($"Hi {userName}, What is your favorite color?");
+var color = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine($"{color} is my favorite too! What is your favorite animal?");
+var animal = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine($"{animal} are cool! What is your favorite band?");
+var band = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine($"{band} makes awesome music!");
+Console.WriteLine();
+Console.WriteLine($"Thank you {userName}, here is your profile.");
+Console.WriteLine("-----------------------------");
+Console.WriteLine($"name: {userName}");
+Console.WriteLine($"favorite color: {color}");
+Console.WriteLine($"favorite animal: {animal}");
+Console.WriteLine($"favorite band: {band}");
+
+
